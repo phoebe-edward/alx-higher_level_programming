ff7564e20611a2dc479ff4da2d14b7fd5130e3e8
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-09-09 08:49:45 -0700

    edited add arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if (len(sys.argv) == 1):
         print("{}".format(0))
     else:
-        sum = 0;
+        sum = 0
         for i in range(1, len(sys.argv)):
             sum += int(sys.argv[i])
         print("{}".format(sum))

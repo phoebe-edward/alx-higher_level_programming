538e6b94119ece018f91bbcf702c899037b00edc
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-09-11 11:57:07 -0700

    print 2D array

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 0:
         print()
         return
-    for 
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j == len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end='')
+            else:
+                print("{:d}".format(matrix[i][j]), end=' ')
+        print()

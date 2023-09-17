b49c982c9fe09a76526965b0f4ae44524d7740e3
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-09-17 12:06:10 -0700

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    max_k = list(a_dictionary)[0]
+    max_v = a_dictionary[max_k]
+    for k, v in a_dictionary.items():
+        if v > max_v:
+            max_k = k
+            max_v = v
+    return max_k

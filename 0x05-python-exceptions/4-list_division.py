2154fca66143158412a61c2bbbe41c817bd267cc
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-09-25 13:42:35 -0700

    task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    res_list = []
+    for i in range(list_length):
+        try:
+            res_list .append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+            res_list.append(0)
+        except (TypeError, ValueError):
+            print("wrong type")
+            res_list.append(0)
+        except IndexError:
+            print("out of range")
+            res_list.append(0)
+    return res_list

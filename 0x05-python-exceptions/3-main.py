56272b1923f1715e556c9a2b75e67fd874f5497e
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-09-25 13:12:17 -0700

    zero division

--- 0x05-python-exceptions/3-main.py
+++ 0x05-python-exceptions/3-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 safe_print_division = __import__('3-safe_print_division').safe_print_division
 
-a = 12
+a = 11
 b = 2
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))

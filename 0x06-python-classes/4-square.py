bd824eb53025ebc059b88c5dd1a50897253b78be
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-09-26 15:00:50 -0700

    setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define Square class."""
+
+
+class Square:
+    """class Square"""
+    def __init__(self, size=0):
+        """initialize square:
+        Args:
+        size(int): the size of the square"""
+        self.__size = size
+
+    def area(self):
+        """calculate area"""
+        return (self.__size)**2
+
+    @property
+    def size(self):
+        """get value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set the value of size
+        based on restrictions"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value

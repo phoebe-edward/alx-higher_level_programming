9cb2c11669c81929d72a612faf0a6e17925f42b3
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-09-26 13:20:43 -0700

    private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define Square class."""
+
+
+class Square:
+    """class Square"""
+    def __init__(self, size):
+        """initialize square:
+        Args:
+        size(int): the size of the square"""
+        self.__size = size

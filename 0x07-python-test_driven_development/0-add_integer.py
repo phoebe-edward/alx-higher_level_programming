61c2625701d515555ead5ec6a5cbcadbf6304f84
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-05 12:53:37 -0700

    test2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,7 +6,7 @@ def add_integer(a, b=98):
     """adds two integers
     a is any integer,
     b is by default 98 unless provided otherwise"""
-    
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):

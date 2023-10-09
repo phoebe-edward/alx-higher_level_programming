8c632eda50a5c7b9a4bf19c9a011ebf7d54824cc
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-09 13:56:00 -0700

    exception from method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        """raises Exception"""
+        raise Exception("area() is not implemented")

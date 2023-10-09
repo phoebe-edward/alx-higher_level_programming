e666d027be1f3e8ece7e5a2c899147a491d872e1
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-09 12:20:24 -0700

    MyList

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class Mylist"""
+
+
+class MyList(list):
+    """class Mylist"""
+
+    def print_sorted(self):
+        """print a sorted list"""
+        print(sorted(self))

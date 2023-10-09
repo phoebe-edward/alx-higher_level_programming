bfff34ffbd8293fd4f4611324c19d29c36ff1e46
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-09 12:49:26 -0700

    is exactly same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Is same class? """
+
+
+def is_same_class(obj, a_class):
+    """returns true if same class"""
+    if type(obj) == a_class:
+        return True
+    return False

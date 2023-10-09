e4762c14c6936b90638c1d678ec0d25215049d0e
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-09 11:59:34 -0700

    edited dir fnc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""function to return attributes and methods of an obj"""
+
+
 def lookup(obj):
     """returns the list of available attributes and methods of an object"""
     return dir(obj)

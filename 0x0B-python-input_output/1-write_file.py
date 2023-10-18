2a0060bdcb74e7e3dd3f46976fb61a0007c65a39
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-18 15:05:31 +0000

    write into file2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """write into file"""
     with open(filename, 'w', encoding="utf-8") as f:
         num = f.write(text)
-        return num 
+        return num

35fc16e4180a94d5bed70fff91f0c61a0c102ad1
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-18 14:50:30 +0000

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""reading from txt file"""
+
+
+def read_file(filename=""):
+    """read file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end='')
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+README inside folder 0x0B-python-input_output

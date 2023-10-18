1298dab4bc57e9c811ea1721938d40c6bba56a56
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-18 15:03:56 +0000

    write into file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""write into txt file"""
+
+
+def write_file(filename="", text=""):
+    """write into file"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        num = f.write(text)
+        return num 

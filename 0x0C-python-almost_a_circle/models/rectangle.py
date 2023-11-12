fd55c2f249607ff2cac37fefd033c8dc5d1c3ab8
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-11-12 17:50:50 +0000

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) not in [int, float]:
+            raise TypeError("width must be real integer or float")
+        elif width <= 0:
+            raise ValueError("width must be positive number")
+        else:
+            self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) not in [int, float]:
+            raise TypeError("width must be real integer or float")
+        elif height <= 0:
+            raise ValueError("width must be positive number")
+        else:
+            self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if type(x) not in [int, float]:
+            raise TypeError("x must be real integer or float")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        if type(y) not in [int, float]:
+            raise TypeError("y must be real integer or float")
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = y

b9d3ce85735aba23cb5b71ebed7ebd52629fa683
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-11-19 16:03:05 +0000

    tests square size

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -17,3 +17,13 @@ class test_square(unittest.TestCase):
         self.assertEqual("[Square] (12) 3/4 - 5", str(s3))
         s3.update(s3.id, 7, 7)
         self.assertEqual("[Square] (12) 3/4 - 7", str(s3))
+
+    def test_square_size(self):
+        s4 = Square(5)
+        self.assertEqual(5, s4.size)
+        s4.size = 7
+        self.assertEqual("[Square] ({}) 0/0 - 7".format(s4.id), str(s4))
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            s4.size = "8"
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            s4.size = -4

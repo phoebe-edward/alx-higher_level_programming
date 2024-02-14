8ab32e052ee51a92daddf45ff911a17cd96b52bd
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2024-02-14 07:07:58 -0800

    unique id

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- create table with unique id
+CREATE TABLE IF NOT EXISTS unique_id(id INTEGER DEFAULT 1 UNIQUE, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql~
@@ -0,0 +1,2 @@
+-- create table
+CREATE TABLE IF NOT EXISTS id_not_null(id INTEGER DEFAULT 1, name VARCHAR(256));

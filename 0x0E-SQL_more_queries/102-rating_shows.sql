3e830e6042b30f7657188aca6e03f5d2df4e54b3
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2024-02-17 08:10:32 -0800

    sum of ratings

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- show all tv_shows sorted by their ratings
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+    FROM tv_shows INNER JOIN tv_show_ratings
+    ON tv_shows.id = tv_show_ratings.show_id
+    GROUP BY tv_shows.title
+    ORDER BY rating DESC;

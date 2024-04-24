947941374751ea9e881032a12c67c9ee7dd04d33
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2024-04-24 09:07:11 -0700

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    let str = '';
+    for (let j = 0; j < parseInt(process.argv[2]); j++) {
+      str = str + 'X';
+    }
+    console.log(str);
+  }
+}
